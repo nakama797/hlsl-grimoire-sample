9c6ae006821b91434342a48076069ffcfd1557df
nakama797 <124228397+nakama797@users.noreply.github.com>
2024-02-07 13:54:28 +0900

    Update main.cpp

--- Sample_02_02/Sample_02_02/main.cpp
+++ Sample_02_02/Sample_02_02/main.cpp
@@ -1,5 +1,8 @@
 #include "stdafx.h"
 #include "system/system.h"
+#define _USE_MATH_DEFINES
+#include <math.h>
+
 
 // 頂点構造体
 struct SimpleVertex
@@ -28,7 +31,6 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLi
     RootSignature rootSignature;
     InitRootSignature(rootSignature);
 
-
     // 2. シェーダーをロード
     Shader vs, ps;
     vs.LoadVS("Assets/shader/sample.fx", "VSMain");
@@ -38,26 +40,36 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLi
     PipelineState pipelineState;
     InitPipelineState(pipelineState, rootSignature, vs, ps);
 
-    // 4. 三角形の頂点バッファを作成
-    // 頂点配列を定義
-    SimpleVertex vertices[] = {
-        {
-            {-0.5f, -0.5f, 0.0f},
-            { 1.0f, 0.0f, 0.0f }
-        },
-        {
-            { 0.0f, 0.5f, 0.0f },
-            { 0.0f, 1.0f, 0.0f }
-        },
-        {
-            { 0.5f, -0.5f, 0.0f },
-            { 0.0f, 0.0f, 1.0f }
-        },
-        {
+    //// 4. 三角形の頂点バッファを作成
+    //// 頂点配列を定義
+    const  int POLYGONAL_NUMBER = 17;	//　多角形の頂点数
+    const float POLYGON_RADIUS = 0.75f;	// 外接円の半径
+    SimpleVertex vertices[POLYGONAL_NUMBER];
+    for (int i = 0; i < POLYGONAL_NUMBER; i++) {
+        vertices[i].pos[0] = cos(2 * M_PI * i / POLYGONAL_NUMBER) * (float)POLYGON_RADIUS * 9.0f / 16.0f;
+        vertices[i].pos[1] = sin(2 * M_PI * i / POLYGONAL_NUMBER) * POLYGON_RADIUS;
+        vertices[i].pos[2] = 0;
+        vertices[i].color[0] = cos(2 * M_PI * i / POLYGONAL_NUMBER);
+        vertices[i].color[1] = sin(2 * M_PI * i / POLYGONAL_NUMBER);
+        vertices[i].color[2] = (float)i / POLYGONAL_NUMBER;
+        //vertices[i].color[3] = 1.0f;
+    }
 
-        }
 
-    };
+    //SimpleVertex vertices[] = {
+    //    {
+    //        {-0.5f, -0.5f, 0.0f},
+    //        { 1.0f, 0.0f, 0.0f }
+    //    },
+    //    {
+    //        { 0.0f, 0.5f, 0.0f },
+    //        { 0.0f, 1.0f, 0.0f }
+    //    },
+    //    {
+    //        { 0.5f, -0.5f, 0.0f },
+    //        { 0.0f, 0.0f, 1.0f }
+    //    }
+    //};
 
     VertexBuffer triangleVB;
     triangleVB.Init(sizeof(vertices), sizeof(vertices[0]));
@@ -65,9 +77,13 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLi
 
     // 5. 三角形のインデックスバッファを作成
     //インデックス配列
-    uint16_t indices[] = {
-        0,1,2,0,2,3
-    };
+    uint16_t indices[(POLYGONAL_NUMBER - 2) * 3];
+    for (int i = 0; i < _countof(indices); i += 3) {
+        indices[i] = 0;
+        indices[i + 1] = (i / 3) + 1;
+        indices[i + 2] = (i / 3) + 2;
+    }
+
     IndexBuffer triangleIB;
     triangleIB.Init(sizeof(indices), 2);
     triangleIB.Copy(indices);
@@ -98,7 +114,7 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLi
         // 5. インデックスバッファを設定
         renderContext.SetIndexBuffer(triangleIB);
         // 6. ドローコール
-        renderContext.DrawIndexed(_countof(indices));
+        renderContext.DrawIndexed(57);
 
         /// //////////////////////////////////////
         // 絵を描くコードを書くのはここまで！！！
@@ -150,5 +166,3 @@ void InitPipelineState(PipelineState& pipelineState, RootSignature& rs, Shader& 
     psoDesc.RTVFormats[0] = DXGI_FORMAT_R8G8B8A8_UNORM;
     pipelineState.Init(psoDesc);
 }
-
-
